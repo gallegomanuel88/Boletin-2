030f53c7acfecdc38268e399927c9f251cefd402
galle <galle@192.168.0.14>
2015-12-13 00:22:46 +0100

    Fin ejercicio

---
+++ Bol2.1/src/bol2/pkg1/Bol21.java
@@ -0,0 +1,12 @@
+package bol2.pkg1;
+public class Bol21 {
+
+    public static void main(String[] args) {
+        float base = 2.0f, area ; // pongo un "f" por ser de tipo float
+        float altura = (float) 3.0; // cast, fuerzas de double a float
+        area = base*altura/2 ;
+        
+        System.out.println("area="+ area);
+    }
+    
+}
