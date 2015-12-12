c8c9888e9fbc441929973ed3769690989ee3b14b
galle <galle@192.168.0.14>
2015-12-13 00:24:45 +0100

    Fin ejercicio

---
+++ Bol2.1b/src/bol2/pkg1b/Bol21b.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol2.pkg1b;
+public class Bol21b {
+
+    public static void main(String[] args) {
+        float base = 2.0f ; // pongo un "f" por ser de tipo float
+        float altura = (float) 3.0; // cast, fuerzas de double a float
+               System.out.println("area="+ base*altura/2);
+    }
+}
